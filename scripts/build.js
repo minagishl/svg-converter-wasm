d136007b2545c4628e6d64cbd9f87661137b6e07
minagishl <mvxd568.co@icloud.com>
2024-07-30 17:02:12 +0900

    Add prettier

--- scripts/build.js
+++ scripts/build.js
@@ -4,35 +4,35 @@ const fs = require('fs');
 const path = require('path');
 
 async function build() {
-	// Remove the dist folder
-	await fs.rmSync(path.join(__dirname, '../dist'), { recursive: true, force: true });
+  // Remove the dist folder
+  await fs.rmSync(path.join(__dirname, '../dist'), { recursive: true, force: true });
 
-	// Bundle TypeScript to JavaScript
-	await esbuild.build({
-		entryPoints: ['src/index.ts'],
-		bundle: true,
-		outfile: 'dist/index.js',
-		platform: 'node',
-		plugins: [dtsPlugin()],
-	});
+  // Bundle TypeScript to JavaScript
+  await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    outfile: 'dist/index.js',
+    platform: 'node',
+    plugins: [dtsPlugin()],
+  });
 
-	// Copy the contents of the pkg folder to dist
-	const pkgDir = path.join(__dirname, '../pkg');
-	const distDir = path.join(__dirname, '../dist');
+  // Copy the contents of the pkg folder to dist
+  const pkgDir = path.join(__dirname, '../pkg');
+  const distDir = path.join(__dirname, '../dist');
 
-	// Create the dist folder if it doesn't exist
-	if (!fs.existsSync(distDir)) {
-		fs.mkdirSync(distDir);
-	}
+  // Create the dist folder if it doesn't exist
+  if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+  }
 
-	fs.readdirSync(pkgDir).forEach((file) => {
-		if (file !== 'package.json') {
-			fs.copyFileSync(path.join(pkgDir, file), path.join(distDir, file));
-		}
-	});
+  fs.readdirSync(pkgDir).forEach((file) => {
+    if (file !== 'package.json') {
+      fs.copyFileSync(path.join(pkgDir, file), path.join(distDir, file));
+    }
+  });
 }
 
 build().catch((e) => {
-	console.error(e);
-	process.exit(1);
+  console.error(e);
+  process.exit(1);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as wasm from '../pkg/svg_converter_wasm';
 
 export function convertSvgToPng(svgData: string): Uint8Array {
-	return wasm.convert_svg_to_png(svgData);
+  return wasm.convert_svg_to_png(svgData);
 }
 
 export function convertSvgToJpeg(svgData: string, quality: number): Uint8Array {
-	return wasm.convert_svg_to_jpeg(svgData, quality);
+  return wasm.convert_svg_to_jpeg(svgData, quality);
 }
