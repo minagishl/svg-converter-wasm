9ea5c7e7526630971ec19925053ab908f2550ddf
minagishl <mvxd568.co@icloud.com>
2024-07-30 17:39:04 +0900

    Add iniSync function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as wasm from '../pkg/svg_converter_wasm';
 
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
 export function convertSvgToPng(svgData: string): Uint8Array {
   return wasm.convert_svg_to_png(svgData);
 }
 
 export function convertSvgToJpeg(svgData: string, quality: number): Uint8Array {
   return wasm.convert_svg_to_jpeg(svgData, quality);
+}
+
+export default function iniSync(module: SyncInitInput): wasm.InitOutput {
+  return wasm.initSync(module);
 }
